f7e776a5f2dd93e94e13f505c135697f8128044a
sir mufasa <jonathankabango08@gmail.com>
2025-09-21 16:25:38 +0200

    something

--- src/Login.jsx
+++ src/Login.jsx
@@ -17,7 +17,7 @@ function Login({ onSuccess }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/login', form);
+      const res = await axios.post('http://54.174.227.60/api/login', form);
       setMessage(res.data.message || 'Login successful');
 
       const user = res.data.user || { email: form.email };
